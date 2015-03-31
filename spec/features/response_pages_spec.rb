a32732e0b7a51a8ecb3b3aab809033f724c69015
Amy Johnson and Mike Goren <amyjohnsonmichelle@gmail.com>
2015-03-31 15:18:04 -0700

    Add ajax to create and display a response

--- spec/features/response_pages_spec.rb
+++ spec/features/response_pages_spec.rb
@@ -13,3 +13,16 @@ describe 'sending email on response' do
     ActionMailer::Base.deliveries.last.to.should eq [user.username]
   end
 end
+describe 'creating a response process' do
+  it 'displays the created response', js: true do
+    user = FactoryGirl.create(:user)
+    user2 = FactoryGirl.create(:user, username: "foo@foo.com")
+    question = FactoryGirl.create(:question, user: user)
+    login(user)
+    visit question_path(question)
+    click_on 'Leave a response'
+    fill_in 'Body', with: 'foo'
+    click_button 'Create Response'
+    expect(page).to have_content "foo"
+  end
+end
